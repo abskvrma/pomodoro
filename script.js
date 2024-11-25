fb429876133a67b51bc63d6be13115a6ffc5d70d
abskvrma <abhishekvermardj5148@gmail.com>
2024-11-25 21:19:12 +0530

    final commit

--- script.js
+++ script.js
@@ -1,33 +1,21 @@
 let sessionCount = document.getElementById("session-count");
 const startBtn = document.getElementById("start");
 const stopBtn = document.getElementById("stop");
-
 const min_timer = document.getElementById("timer-min");
 const sec_timer = document.getElementById("timer-sec");
-
 let todo = document.getElementById("todo");
 console.log(todo);
-
 let session_count=1;
 let min_count = 25;
 let sec_count = 0;
-
 let flag = 0;
-
-
 function breakTimer(){
      
     flag=0;
 }
-
 function workTimer(){
     
 }
-
-
-
-
-
 startBtn.addEventListener("click",()=>{
     sessionCount.innerText=session_count.toString();
     todo.innerText="!!Work Time!!";
@@ -36,7 +24,6 @@ startBtn.addEventListener("click",()=>{
         if(sec_count==0) {
             min_count--;
             sec_count=60;
-
             if(min_count<=0){
                 console.log("hello");
                 
@@ -60,23 +47,16 @@ startBtn.addEventListener("click",()=>{
                 sessionCount.innerText=session_count.toString();
     
             }
-
-
         }
          else{
         sec_count--;
         sec_timer.innerText=sec_count.toString();
         min_timer.innerText=min_count.toString();}
-
-
     },1000);
-
     stopBtn.addEventListener("click",()=>{
-
         clearInterval(intervalId);
         min_timer.innerText=25;
         sec_timer.innerText=0;
     })
-
    
 })
